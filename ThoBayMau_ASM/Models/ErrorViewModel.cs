a55ec78b037d3ee7c2d99df239dded089a08549d
ho dung <dungho2209@gmail.com>
2024-03-13 10:34:42 +0700

    Add project files.

---
+++ ThoBayMau_ASM/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ThoBayMau_ASM.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
