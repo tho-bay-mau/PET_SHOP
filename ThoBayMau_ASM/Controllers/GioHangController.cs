3515c3b48d032a4032a113cdf13e4a777ce7923d
TUAN <hoangquoctuans3@gmail.com>
2024-03-19 23:12:12 +0700

    them vao gio hang

--- ThoBayMau_ASM/Controllers/GioHangController.cs
+++ ThoBayMau_ASM/Controllers/GioHangController.cs
@@ -1,12 +1,58 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Aram.Infrastructure;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Policy;
+using ThoBayMau_ASM.Data;
+using ThoBayMau_ASM.Models;
 
 namespace ThoBayMau_ASM.Controllers
 {
     public class GioHangController : Controller
     {
-        public IActionResult Index()
+		private readonly ThoBayMau_ASMContext _context;
+
+		public GioHangController(ThoBayMau_ASMContext context)
+		{
+			_context = context;
+		}
+		public GioHang? GioHang { get; set; }
+		public IActionResult Index()
         {
-            return View();
+			GioHang = HttpContext.Session.GetJson<GioHang>("giohang") ?? new GioHang();
+			return View(GioHang);
         }
-    }
+		[HttpPost]
+		public IActionResult AddToGioHang(int sanPhamId, int? quantity, int? kichThuoc, string? returnUrl)
+		{
+			/*SanPham? sanPham = _context.SanPham
+				.Where(x => x.Id == sanPhamId)
+				.Include(x => x.ChiTietSPs)
+				.FirstOrDefault(x => x.ChiTietSPs.Any(x => x.KichThuoc == kichThuoc));*/
+			ChiTiet_SP? chiTietSP = _context.ChiTiet_SP
+				.Include(x => x.SanPham)
+				.FirstOrDefault(s => s.KichThuoc == kichThuoc && s.SanPhamId == sanPhamId);
+			if (chiTietSP != null)
+			{
+				GioHang = HttpContext.Session.GetJson<GioHang>("giohang") ?? new GioHang();
+				if (quantity != null)
+				{
+					GioHang.AddItem(chiTietSP, (int)quantity);
+				}
+				else
+				{
+					GioHang.AddItem(chiTietSP, 1);
+				}
+				HttpContext.Session.SetJson("giohang", GioHang);
+			}
+
+			if (string.IsNullOrEmpty(returnUrl))
+			{
+				return View("Index", GioHang);
+			}
+			else
+			{
+				return Redirect(returnUrl);
+			}
+		}
+	}
 }
