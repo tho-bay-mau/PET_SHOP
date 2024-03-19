4c4c3e3d82d58872d001a43d0e9fb6a8bd228804
TUAN <hoangquoctuans3@gmail.com>
2024-03-19 20:43:16 +0700

    dsd

--- ThoBayMau_ASM/Controllers/HomeController.cs
+++ ThoBayMau_ASM/Controllers/HomeController.cs
@@ -22,9 +22,44 @@ namespace ThoBayMau_ASM.Controllers
             var result = _db.SanPham.Include(x => x.ChiTietSPs).Include(x => x.Anhs).ToList();
 			return View(result);
 		}
-        public IActionResult product_detail()
+		public IActionResult GetImage(int? id_sp, int? id_hinh)
+		{
+			if (id_hinh != null)
+			{
+				var hinh = _db.Anh
+				.FirstOrDefault(x => x.Id == id_hinh);
+				var imageBytes = System.IO.File.ReadAllBytes("wwwroot/img/" + hinh.TenAnh); // Đọc file ảnh thành mảng byte
+				return File(imageBytes, "image/jpeg");
+			}
+			else if (id_sp != null)
+			{
+				var hinh = _db.Anh
+					.FirstOrDefault(x => x.SanphamId == id_sp);
+				if (hinh != null)
+				{
+                    var imageBytes = System.IO.File.ReadAllBytes("wwwroot/img/" + hinh.TenAnh);
+					// Đọc file ảnh thành mảng byte
+					return File(imageBytes, "image/jpeg");
+				}
+				else
+				{
+					return NotFound();
+				}
+
+			}
+			else
+			{
+				return NotFound();
+			}
+		}
+		public IActionResult product_detail(int id_sp)
         {
-            return View();
+            var result = _db.SanPham
+					.Include(x => x.ChiTietSPs)
+					.Include(x => x.Anhs)
+					.Where(x => x.Id == id_sp)
+					.ToList();
+            return View(result);
         }
         public IActionResult Cart()
         {
