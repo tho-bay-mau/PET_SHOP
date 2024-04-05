3d4b80935ecaabefab6acb95b858af29a3429874
TUAN <hoangquoctuans3@gmail.com>
2024-04-05 14:13:20 +0700

    sua : don hang admin

--- ThoBayMau_ASM/Controllers/DonHangController.cs
+++ ThoBayMau_ASM/Controllers/DonHangController.cs
@@ -67,7 +67,7 @@ namespace ThoBayMau_ASM.Controllers
                 .ThenInclude(x => x.SanPham)
                 .ThenInclude(x => x.LoaiSP)
                 .Skip((currentpage - 1) * ITEM_PER_PAGE).Take(ITEM_PER_PAGE)
-                .OrderByDescending(x => x.ThoiGianTao)
+                .OrderByDescending(x => x.ThoiGianHuy == null ? x.ThoiGianTao : x.ThoiGianHuy)
                 .ToList();
                 int count = _context.DonHang.Where(x => x.TrangThaiDonHang == "cho duyet").Count();
                 ViewBag.Count = count;
@@ -96,9 +96,11 @@ namespace ThoBayMau_ASM.Controllers
                         if(DonHang.TrangThaiDonHang == "cho duyet")
                         {
                             DonHang.TrangThaiDonHang = "dang giao";
+                            DonHang.ThoiGianHuy = DateTime.Now;
                         } else
                         {
                             DonHang.TrangThaiDonHang = "da giao";
+                            DonHang.ThoiGianHuy = DateTime.Now;
                         }
                         _context.Update(DonHang);
                         _context.SaveChanges();
