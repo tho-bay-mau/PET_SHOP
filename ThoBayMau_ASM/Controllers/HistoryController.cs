02a5162adeb48cab5c47e27e84e584e7eeb2fd0c
Do Hoang Phong <hoang@MSIGF63THIN10SC>
2024-04-12 14:00:27 +0700

    update history and layout

---
+++ ThoBayMau_ASM/Controllers/HistoryController.cs
@@ -0,0 +1,106 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ThoBayMau_ASM.Data;
+
+namespace ThoBayMau_ASM.Controllers
+{
+    public class HistoryController : Controller
+    {
+        private readonly ThoBayMau_ASMContext _db;
+
+        public HistoryController(ThoBayMau_ASMContext db)
+        {
+            _db = db;
+        }
+
+        public const int ITEM_PER_PAGE = 7;
+
+        [BindProperty(SupportsGet = true, Name = "p")]
+        public int currentpage { get; set; }
+
+        public int countpages { get; set; }
+        public IActionResult Index()
+        {
+            ViewBag.LichSu = true;
+            int total = _db.LichSu.Count();
+            countpages = (int)Math.Ceiling((double)total / ITEM_PER_PAGE);
+
+            if (currentpage < 1)
+            {
+                currentpage = 1;
+            }
+            if (currentpage > countpages)
+            {
+                currentpage = countpages;
+            }
+
+            ViewBag.CurrentPage = currentpage;
+            ViewBag.CountPages = countpages;
+            if (total > 0)
+            {
+                var result = _db.LichSu.OrderByDescending(x => x.NgayGio).Skip((currentpage - 1) * ITEM_PER_PAGE).Take(ITEM_PER_PAGE).ToList();
+                return View(result);
+            }
+            else
+            {
+                return View(null);
+            }
+        }
+        public IActionResult Search(string Key)
+        {
+            ViewBag.LichSu = true;
+            if (Key != null)
+            {
+                int total = _db.LichSu.Where(x => x.ThongTin_ThaoTac.ToLower().Contains(Key.ToLower())).Count();
+                countpages = (int)Math.Ceiling((double)total / ITEM_PER_PAGE);
+
+                if (currentpage < 1)
+                {
+                    currentpage = 1;
+                }
+                if (currentpage > countpages)
+                {
+                    currentpage = countpages;
+                }
+
+                ViewBag.CurrentPage = currentpage;
+                ViewBag.CountPages = countpages;
+                ViewBag.Search = Key;
+                if (total > 0)
+                {
+                    var result = _db.LichSu.Where(x => x.ThongTin_ThaoTac.ToLower().Contains(Key.ToLower())).OrderByDescending(x => x.NgayGio).Skip((currentpage - 1) * ITEM_PER_PAGE).Take(ITEM_PER_PAGE).ToList();
+                    return View("Index", result);
+                }
+                else
+                {
+                    return View("Index", null);
+                }
+            }
+            else
+            {
+                int total = _db.LichSu.Count();
+                countpages = (int)Math.Ceiling((double)total / ITEM_PER_PAGE);
+
+                if (currentpage < 1)
+                {
+                    currentpage = 1;
+                }
+                if (currentpage > countpages)
+                {
+                    currentpage = countpages;
+                }
+
+                ViewBag.CurrentPage = currentpage;
+                ViewBag.CountPages = countpages;
+                if (total > 0)
+                {
+                    var result = _db.LichSu.OrderByDescending(x => x.NgayGio).Skip((currentpage - 1) * ITEM_PER_PAGE).Take(ITEM_PER_PAGE).ToList();
+                    return View("Index", result);
+                }
+                else
+                {
+                    return View("Index", null);
+                }
+            }
+        }
+    }
+}
--- ThoBayMau_ASM/Models/LichSu.cs
+++ ThoBayMau_ASM/Models/LichSu.cs
@@ -7,11 +7,16 @@ namespace ThoBayMau_ASM.Models
 	public class LichSu
 	{
 		[Key]
-		public int Id { get; set; }
-		public string ThongTin_ThaoTac { get; set; }
-		public DateTime NgayGio {  get; set; }
-		public string ChiTiet { get; set; }
-		public int TaiKhoanId { get; set; }
+        [Display(Name = "ID")]
+        public int Id { get; set; }
+        [Display(Name = "Thao tác")]
+        public string ThongTin_ThaoTac { get; set; }
+        [Display(Name = "Thời gian")]
+        public DateTime NgayGio {  get; set; }
+        [Display(Name = "Chi tiết")]
+        public string ChiTiet { get; set; }
+        [Display(Name = "ID tài khoản thao tác")]
+        public int TaiKhoanId { get; set; }
 		public TaiKhoan TaiKhoan { get; set; }
 	}
 }
